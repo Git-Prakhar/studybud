9a1bcb4612d58125711f60bb0eb1f4b6c06b5a1d
Git-Prakhar <kprakhar01@gmail.com>
2024-09-18 19:39:59 +0530

    Learned more of Templates

--- base/views.py
+++ base/views.py
@@ -1,11 +1,93 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    return render(request, 'home.html', {
-        'first_name': 'John',
-        'last_name': 'Doe',
-    })
 
-def room(request):
-    return render(request, 'room.html')
+rooms = [
+    {
+        'id': 1,
+        'name' : 'Conference Room A',
+    },
+    {
+        'id': 2,
+        'name' : 'Home Group Room',
+    },
+    {
+        'id': 3,
+        'name' : 'Workshop Room',
+    },
+    {
+        'id': 4,
+        'name' : 'Meeting Room',
+    },
+    {
+        'id': 5,
+        'name' : 'Seminar Room',
+    },
+    {
+        'id': 6,
+        'name' : 'Board Room',
+    },
+    {
+        'id': 7,
+        'name' : 'Training Room',
+    },
+    {
+        'id': 8,
+        'name' : 'Classroom',
+    },
+    {
+        'id': 9,
+        'name' : 'Lecture Room',
+    },
+    {
+        'id': 10,
+        'name' : 'Auditorium',
+    },
+    {
+        'id': 11,
+        'name' : 'Exhibition Hall',
+    },
+    {
+        'id': 12,
+        'name' : 'Ballroom',
+    },
+    {
+        'id': 13,
+        'name' : 'Banquet Hall',
+    },
+    {
+        'id': 14,
+        'name' : 'Function Room',
+    },
+    {
+        'id': 15,
+        'name' : 'Party Room',
+    },
+    {
+        'id': 16,
+        'name' : 'Event Space',
+    },
+    {
+        'id': 17,
+        'name' : 'Studio',
+    },
+    {
+        'id': 18,
+        'name' : 'Office Space',
+    },
+    {
+        'id': 19,
+        'name' : 'Co-working Space',
+    },
+    {
+        'id': 20,
+        'name' : 'Hot Desk',
+    }
+]
+
+def home(request):
+    return render(request, 'base/home.html', {'rooms': rooms})
+
+def room(request, pk):
+    context = {'roomName': rooms[int(pk) - 1], 'rooms': rooms, 'pk': pk}
+    return render(request, 'base/room.html', context)
